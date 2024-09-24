221652207b475ae15fbc3d3cd9a6ae10c3093e56
Matthew Sigmond <43422956+matthew28845@users.noreply.github.com>
2024-09-23 20:45:27 -0500

    hwb progress

--- assignmentb/main.scm
+++ assignmentb/main.scm
@@ -3,5 +3,18 @@
 (define sum
   (lambda (nums)
     (if (null? nums)
-      0 
-       (+ (car nums) sum (cdr nums)))))
+      0
+       (+ (car nums) (sum (cdr nums))))))
+
+;Input n: An integer.
+;Input lyst: A list.
+;Return: A list consisting of the first n items from lyst.
+;If n is 0, then the empty list is returned.
+;If n is negative or greater than the length of lyst, then an empty list is returned.
+(define keep-first-n
+  (lambda (n lyst)
+    (if (> 1 n)
+      '()
+        (if (null? lyst)
+          '()
+          (cons (car lyst) (keep-first-n (- n 1) (cdr lyst)))))))
