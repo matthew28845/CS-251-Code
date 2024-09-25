896a48b4be3321531427f4d015db6e663b67162f
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-09-24 21:02:31 -0500

    Update main.scm

--- bst-main/main.scm
+++ bst-main/main.scm
@@ -2,19 +2,19 @@
 ;Return: The integer key stored in bst's root node (or #f if bst is empty).
 (define entry
   (lambda (bst)
-    (car bst)))
+    (if (equal? (check-bst bst) #f) #f (car bst))))
 
 ;Input bst: A binary search tree (possibly empty).
 ;Return: The left subtree of bst's root node (or #f if bst is empty).
 (define left
   (lambda (bst)
-    (car (cdr bst))))
+    (if (equal? (check-bst bst) #f) #f (car (cdr bst)))))
 
 ;Input bst: A binary search tree (possibly empty).
 ;Return: The right subtree of bst's root node (or #f if bst is empty).
 (define right
   (lambda (bst)
-    (car (cdr (cdr bst)))))
+    (if (equal? (check-bst bst) #f) #f (car (cdr (cdr bst))))))
 
 ;Input elt: An integer.
 ;Input left-bst: A binary search tree (possibly empty).
@@ -29,19 +29,19 @@
 ;Return: A list of all entries in bst, ordered according to a preorder traversal.
 (define preorder
   (lambda (bst)
-    ...))
+    #f))
 
 ;Input bst: A binary search tree (possibly empty).
 ;Return: A list of all entries in bst, ordered according to a inorder traversal.
 (define inorder
   (lambda (bst)
-    ...))
+    #f))
 
 ;Input bst: A binary search tree (possibly empty).
 ;Return: A list of all entries in bst, ordered according to a postorder traversal.
 (define postorder
   (lambda (bst)
-    ...))
+    #f))
 
 ;Input v: An integer.
 ;Input bst: A binary search tree (possibly empty).
@@ -49,4 +49,15 @@
 ;already contained v, then this result is identical to bst.
 (define insert
   (lambda (v bst)
-    ...))
+    #f))
+
+;Input bst: A binary search tree (to validate.)
+;Return: the bst if it is valid, #f if not
+(define check-bst
+  (lambda (bst)
+    (if (integer? (car bst))
+      (if (list? (car (cdr bst)))
+        (if (list? (car (cdr (cdr bst))))
+          #t
+          #f) #f) (if (null? (car bst)) #t
+                          ))))
