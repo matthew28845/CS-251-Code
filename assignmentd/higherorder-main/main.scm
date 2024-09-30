168716ec630370e0a5f8335af1d477428ded90b2
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2024-09-29 21:23:09 -0500

    finish assignment d

--- assignmentd/higherorder-main/main.scm
+++ assignmentd/higherorder-main/main.scm
@@ -13,14 +13,16 @@
 ;((uncurry3 (curry3 f)) x y z) is equivalent to (f x y z).
 (define uncurry3
   (lambda (c)
-    #f))
+    (lambda (x y z)
+      (((c x) y) z))))
 
 ;Input pred: A function of one argument that returns a Boolean.
 ;Input lyst: A list.
 ;Return: A list consisting of all list elements for which pred returns #t.
 (define my-filter
   (lambda (pred lyst)
-    ...))
+    (if (null? lyst) (list)
+      (if (equal? #t (pred (car lyst))) (append (list (car lyst)) (my-filter pred (cdr lyst))) (my-filter pred (cdr lyst))))))
 
 ;Input pred: A function of one argument that returns a Boolean.
 ;Input lyst: A list.
@@ -28,7 +30,8 @@
 ;elements of the list evaluate to #f under f.
 (define exists
   (lambda (pred lyst)
-    ...))
+    (if (null? lyst) #f
+      (if (equal? #t (pred (car lyst))) #t (exists pred (cdr lyst))))))
 
 ;Input symb: A symbol.
 ;Input docs: A list of key-value pairs. A key is a symbol. A value is a list of symbols.
@@ -36,4 +39,6 @@
 ;that key.
 (define find-docs
   (lambda (symb docs)
-    ...))
+    (map car 
+	 (filter (lambda (doc) (exists (lambda (x) (equal? symb x)) (car (cdr doc)))) 
+		 docs))))
